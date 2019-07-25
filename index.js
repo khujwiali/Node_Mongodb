c0c2fc7e2e2a8b2af6e47f72b2c317738f828bdf
Mohammed Ali Khujwi <mohdalikhujwi@credenceanalytics.com>
2019-07-25 12:56:04 +0530

    CRUD Api

--- index.js
+++ index.js
@@ -1,41 +1,64 @@
 const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
 
-const uri = "mongodb+srv://khujwiali:Newuser123@khujwi-cluster-xfido.mongodb.net/test?retryWrites=true&w=majority";
+dotenv.config();
+app.use(bodyParser.urlencoded({extended:true}));
+const uri = process.env.MONGODB_URI;
+let db;
 
-// MongoClient.connect(uri,function(err,client){
-//     console.log("DB Connected",db);
-//     var cursor = db.collection('quotes').find()  
-//     // db.close();
-// })
-var db;
 MongoClient.connect(uri, (err, client) => {
-    if (err) return console.log(err)
-    console.log("Connected")
-    db = client.db('MoviesDB') // whatever your database name is
+  if (err) return console.log(err)
+  console.log("Connected")
+  db = client.db('MoviesDB') // whatever your database name is
 
-    app.listen(3000, () => {
-      console.log('listening on 3000')
+  app.listen(process.env.PORT, () => {
+    console.log('listening on '+process.env.PORT);
     })
-  })
-
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   console.log("DB Connected")
-//   console.log("collection",collection);
-//   client.close();
-// });
-
-app.get('/',(req,res)=>{
-    db.collection('Movie').find().toArray(function(err, results) {
-        console.log(results)
-        // send HTML file populated with quotes here
-      })
-    // console.log("cursor",cursor)
-    res.send("cursor")
 });
 
-// app.listen('3001',()=>console.log("Node Express Sever Started At PORT : 3001"));
+app.get('/read',(req,res)=>{
+  db.collection('Movie').find().toArray(function(err, results) {
+    if(err) return console.log(err)
+    console.log(results)
+    res.send(results);
+  })
+});
+
+app.post('/create',(req,res)=>{
+  console.log(req.body);
+  db.collection('Movie').save(req.body,(err, result)=>{
+    if(err) return console.log(err)
+    console.log('Created Successfully');
+    res.send('Created Successfully');
+  })
+});
+
+app.put('/update', (req, res) => {
+  if(!req.query) return res.send("No key found for update")
+  console.log("movie_id",req.query.movie_id);
+  console.log("req.body",req.body);
+  db.collection('Movie')
+  .findOneAndUpdate({movie_id: req.query.movie_id}, {
+    $set: {
+      name: req.body.name
+    }
+  }, {
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err)
+    console.log("put result",result)
+    res.send("Updated Successfully");
+  })
+})
+
+app.delete('/delete', (req, res) => {
+  db.collection('Movie').findOneAndDelete({movie_id: req.body.movie_id},
+  (err, result) => {
+    if (err) return res.send(500, err)
+    console.log("delete result",result);
+    res.send("Deleted Successfully");
+  })
+})
